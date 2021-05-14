5f77d01fbca8850f498027bebde04560a891c178
ataryan4561 <ataryan4561@gmail.com>
2021-05-15 01:24:03 +0530

    sign in update

---
+++ src/pages/auth/signin.vue
@@ -0,0 +1,44 @@
+<template>
+  <f7-page name="about">
+    <f7-navbar title="Sign in" back-link="Back"></f7-navbar>
+    <f7-list no-hairlines-md>
+
+ <f7-list-input :value="email" @input="email=$event.target.value"
+    label="E-mail"
+    type="email"
+    placeholder="Your e-mail"
+    clear-button
+  >
+  </f7-list-input>
+
+  <f7-list-input :value="password" @input="password=$event.target.value"
+    label="Password"
+    type="password"
+    placeholder="Your password"
+    clear-button
+  >
+  </f7-list-input>
+
+ 
+</f7-list>
+<f7-block>
+<f7-button outline>Sign in</f7-button>
+<br>
+<div style="text-allign:center;">
+<f7-link>Resend Confirmation Email</f7-link><br>
+<f7-link>Don't have an account? Sign in</f7-link><br>
+<f7-link>Forget Password</f7-link>
+</div>
+</f7-block>
+  </f7-page>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      email:null,
+      password:null
+    }
+  }
+}
+</script>
